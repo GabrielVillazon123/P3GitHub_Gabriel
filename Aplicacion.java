f72ea1774923f5dd3a20f1780494c1a94af4a009
Alex <155954131+JFan015@users.noreply.github.com>
2024-03-08 12:11:33 +0100

    NuevasFuncionalidades
    Agregamos nuevas funcionalidades a los dos programas

--- Aplicacion.java
+++ Aplicacion.java
@@ -4,8 +4,11 @@ public class Aplicacion {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("\nCual es tu nombre?");
+        System.out.println("\n¿Cual es tu nombre?");
         String nombre = scanner.nextLine();
         System.out.println("Hola " + nombre + "!");
+        System.out.println("\nIntroduce tu edad");
+        int edad = scanner.nextInt();
+        System.out.println(nombre+ " tienes una edad de "+edad );
     }
 }
